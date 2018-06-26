d40ce7b49e3afa2ce7c26d78e3d1014ddaced568
Petter Reinholdtsen <pere@hungry.com>
2018-06-26 13:57:42 +0200

    Add header to library file.

--- BTCtrader/__init__.py
+++ BTCtrader/__init__.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 Petter Reinholdtsen <pere@hungry.com>
+# This file is covered by the GPLv2 or later, read COPYING for details.
+
+__version__ = "0.0.0"
+__author__ = "Petter Reinholdtsen <pere@hungry.com>"
+
 """
 http://api.bitcoincharts.com/v1/markets.json
 http://api.hitbtc.com/api/1/public/BTC$cur/ticker
