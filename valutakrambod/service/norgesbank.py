b2c33836c08d36634cab6f20e9fcba7668e87138
Petter Reinholdtsen <pere@hungry.com>
2021-02-27 08:31:03 +0100

    Fixed RSS parsing from norgesbank.

--- valutakrambod/service/norgesbank.py
+++ valutakrambod/service/norgesbank.py
@@ -33,7 +33,8 @@ daily.  See also https://www.norges-bank.no/RSS/.
     async def fetchRates(self, pairs = None):
         if pairs is None:
             pairs = self.ratepairs()
-        eururl = '%sRSS/Euro-EUR---dagens-valutakurs-fra-Norges-Bank/' % self.baseurl
+        eururl = '%sRSS/euro-eur---dagens-valutakurs-fra-norges-bank/' % self.baseurl
+
         usdurl = '%sRSS/Amerikanske-dollar-USD---dagens-valutakurs-fra-Norges-Bank/' % self.baseurl
         res = {}
         for pair in pairs:
@@ -46,17 +47,17 @@ daily.  See also https://www.norges-bank.no/RSS/.
             rss =  etree.fromstring(body)
             item = rss.xpath('/rss/channel/item')[0]
             title = item.xpath("./title/text()")[0]
-            # Note, pubdate from this source can not be trusted.  It
+            # Note, old pubdate from this source can not be trusted.  It
             # was seen to move back in time, from 17:00 GMT to 03:00
             # GMT, and is ignored because of this.
-            pubdate = item.xpath("./pubDate/text()")[0]
+            date = item.xpath("./@Date")[0]
             m = re.match("1 %s = ([0-9.]+) NOK (\d{4}-\d{2}-\d{2}) Norges Banks midtkurs" % pair[0], title)
             if m:
                 r = Decimal(m.group(1))
                 # Hardcode 16:00 CET based on information from
                 # https://www.norges-bank.no/Statistikk/Valutakurser/
                 when = self.datestr2epoch("%s 16:00 CET" % m.group(2))
-                #print(title, pubdate, when)
+                #print(title, date, when)
                 self.updateRates(pair, r, r, when)
                 res[pair] = self.rates[pair]
             else:
